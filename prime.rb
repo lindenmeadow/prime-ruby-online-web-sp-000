d95f3d58a850225f1df1a0359a9f8676af48fa8d
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2019-06-02 19:53:30 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -7,7 +7,7 @@ def prime?(integer)
   elsif integer > 3
     if integer.even?
       false
-    elsif integer.odd? && integer % integer == 0 && integer % 1 == 0 && integer % 3 != 0
+    elsif integer.odd? && integer / integer == 1 && integer % 1 == integer && integer % 3 != 0
       true
     elsif
       false
