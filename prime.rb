95d109578fb90cac3d18bd376d4d4a51d730f974
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2019-06-02 20:24:09 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,16 +1,4 @@
 # Add  code here!
 def prime?(n)
-  if n < 2
-    false
-  if n == 2 || n == 3
-    true
-  if  n % 2 == 0 || n % 3 == 0
-    false
-  i = 5
-  while i * i <= n
-    if n % i == 0 || n % (i + 2) == 0
-      false
-    i += 6
-    true
-  end
+  return num <= 3 or (num + 1) % 6 == 0 or (num - 1) % 6 == 0
 end
