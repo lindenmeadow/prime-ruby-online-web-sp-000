870edd0ba041ea89f3ed2d78cf85d0cab7bf1d3c
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2019-06-02 21:34:51 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,4 @@
 # Add  code here!
 def prime?(num)
-  if num <= 1
-    false
-  elsif num == 2 || num == 3
-    true
-  elsif (2..n-1).to_a.all? do |integer|
-     n % integer != 0
-     end
-    end
+  
 end
