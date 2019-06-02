efa7d2595a0c3435a5f7baa55dd621e55a847281
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2019-06-02 20:17:23 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,16 +1,12 @@
 # Add  code here!
-def prime?(integer)
-  if integer <= 1
+def prime?(n)
+  if n < 2
     false
-  elsif integer == 2 || integer == 3
+  if n == 2 || n == 3
     true
-  elsif integer > 3
-    if integer.even?
-      false
-    elsif integer.odd? && integer % integer == 0 && integer % 1 == 0
-      true
-    else
-      false
-    end
-  end
+  if  n % 2 == 0 || n % 3 == 0
+    false
+  i = 5
+  
+  
 end
