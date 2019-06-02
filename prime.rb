1b80d73b17bee70327cdd881b7f82c8f5054a876
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2019-06-02 19:22:45 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,6 @@
 # Add  code here!
 def prime?(integer)
-  if integer > 0 && integer % integer == 0 && integer % 1 == 0
+  if integer > 0 && integer > 1 && integer % integer == 0 && integer % 1 == 0
     return true
   else
     return false
